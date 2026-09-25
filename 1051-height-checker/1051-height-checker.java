a69d4ebed8411e220dc475ee867fd214e7ec59e4
Saurav Kumar Saini <sainisaurav2503@gmail.com>
2026-09-26 00:56:22 +0530

    Time: 0 ms (100%), Space: 43.2 MB (74.24%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,14 +1,20 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int[] expected = heights.clone();
-        Arrays.sort(expected);
+        int[] freq = new int[101];
+        for (int x : heights) {
+            freq[x]++;
+        }
         int count = 0;
+        int expected = 1;
         for (int i = 0; i < heights.length; i++) {
-            if (heights[i] != expected[i]) {
+            while (freq[expected] == 0) {
+                expected++;
+            }
+            if (heights[i] != expected) {
                 count++;
             }
+            freq[expected]--;
         }
         return count;
-        
     }
 }
